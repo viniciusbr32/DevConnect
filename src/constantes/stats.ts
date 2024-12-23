9997f63f6a3656eab87c4db6384014c10609a43a
Luiz Vinicius <viniciusgba2008@gmail.com>
2024-12-23 18:55:19 -0300

    feat: componente stats

---
+++ src/constantes/stats.ts
@@ -0,0 +1,10 @@
+interface Stats {
+	value: string;
+	label: string;
+}
+
+export const stats: Stats[] = [
+	{ value: "2.5K+", label: "Desenvolvedores" },
+	{ value: "500+", label: "Projetos Criados" },
+	{ value: "150+", label: "Projetos Concluídos" },
+];
