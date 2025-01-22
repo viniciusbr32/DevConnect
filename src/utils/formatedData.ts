56955f1ed4e8b850ca0a2a6a0bf6c435d9fb5c74
Luiz Vinicius <viniciusgba2008@gmail.com>
2025-01-21 22:47:07 -0300

    feat: função para formatar data

--- src/utils/formatedData.ts
+++ src/utils/formatedData.ts
@@ -1,4 +1,10 @@
-import { formatDistanceToNow, parseISO } from "date-fns";
+import {
+	differenceInDays,
+	format,
+	formatDistanceToNow,
+	parseISO,
+	startOfDay,
+} from "date-fns";
 import { ptBR } from "date-fns/locale";
 
 export function formatPublishedDate(isoDate: string) {
@@ -11,4 +17,21 @@ export function formatPublishedDate(isoDate: string) {
 	const cleanDistance = distance.replace("cerca de ", "");
 
 	return `Publicado ${cleanDistance}`;
+}
+
+export function FormatedDate(isoDate: string) {
+	const created = parseISO(isoDate);
+	const dateFormated = format(created, "dd MMM  yyyy ", {
+		locale: ptBR,
+	});
+	return dateFormated;
+}
+
+export function FormatDifarenceDays(isoDate: string) {
+	const deadline = parseISO(isoDate);
+	const today = parseISO(new Date().toISOString());
+
+	const distance = differenceInDays(deadline, today);
+
+	return distance;
 }
